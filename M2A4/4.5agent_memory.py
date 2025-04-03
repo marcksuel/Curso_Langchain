e2409c6fe9871c2e9e4854c785688ba182e88998
Tiago Brasileiro <brasileiroaraujo@gmail.com>
2025-04-03 13:39:51 -0300

    Module2

--- M2A4/4.5agent_memory.py
+++ M2A4/4.5agent_memory.py
@@ -7,16 +7,6 @@ from langchain_community.utilities import WikipediaAPIWrapper
 
 from M2A3.utils import llm_loader
 
-
-def explicar_conceito(conceito: str) -> str:
-    return f"'{conceito}' é um conceito essencial. Recomenda-se começar pelos fundamentos teóricos e exemplos práticos."
-
-def resumir_texto(texto: str) -> str:
-    return f"Resumo: {texto}..."
-
-def sugerir_material(tema: str) -> str:
-    return f"Sugestão: Consulte o livro 'Introdução a {tema}' da editora Universitária."
-
 wiki_api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=250)
 wikipedia = WikipediaQueryRun(description="A tool to explain things in text format. Use this tool if you think the user’s asked concept is best explained through text.", api_wrapper=wiki_api_wrapper)
 
@@ -24,17 +14,20 @@ tools = [
     wikipedia
 ]
 
+# Configurando a memória
 memory = ChatMessageHistory(session_id="test-session")
 
+# Criando a LLM
 llm = llm_loader.llm_mistral()
 
-
+# Carregando o prompt via Hub, lib com vários modelos de prompts
 prompt = hub.pull("hwchase17/react")
 
-
+# Criação dos agentes
 agent = create_react_agent(llm, tools, prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools)
 
+# Criando o executável com memória
 agent_with_chat_history = RunnableWithMessageHistory(
     agent_executor,
     lambda session_id: memory,
@@ -44,7 +37,7 @@ agent_with_chat_history = RunnableWithMessageHistory(
 
 # primeira pergunta
 resposta = agent_with_chat_history.invoke(
-    {"input": "Quantas pessoas vivem no Brasil?"},
+    {"input": "O que é um banco NoSQL?"},
     config={"configurable": {"session_id": "test-session"}}
 )
 print("Resposta do agente:\n", resposta)
@@ -52,7 +45,7 @@ print("Resposta do agente:\n", resposta)
 
 # segunda pergunta
 resposta = agent_with_chat_history.invoke(
-    {"input": "Como é chamado seu hino nacional?"},
+    {"input": "Cite alguns exemplos desse tipo de banco-."},
     config={"configurable": {"session_id": "test-session"}}
 )
 print("Resposta do agente:\n", resposta)
